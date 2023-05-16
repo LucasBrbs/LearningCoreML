6f7ef924e410d0ea6588723254d674bf53014466
Lucas Barbosa de Oliveira <lucasbarbosa2807@gmail.com>
2023-05-16 08:52:27 -0300

    Initial Commit

---
+++ LearningCoreML/LearningCoreMLApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningCoreMLApp.swift
+//  LearningCoreML
+//
+//  Created by Lucas Barbosa de Oliveira on 16/05/23.
+//
+
+import SwiftUI
+
+@main
+struct LearningCoreMLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
